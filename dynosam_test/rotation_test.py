ba7579a246a091af1348ba0029c42b75d4081526
Yiduo Wang <yiduowang47@gmail.com>
2023-11-01 13:10:35 +1100

    Fix frame convention and successfully compute rotational component

--- dynosam_test/rotation_test.py
+++ dynosam_test/rotation_test.py
@@ -57,6 +57,14 @@ def transform_points(points, motion):
     print("We expect 3D points")
     print(points_shape)
 
+def point_frame_robotics_to_cv(points):
+  points_tmp = np.copy(points)
+  points_camera = np.copy(points)
+  points_camera[0, :] = -points_tmp[1, :]
+  points_camera[1, :] = -points_tmp[2, :]
+  points_camera[2, :] = points_tmp[0, :]
+  return points_camera
+
 def project_points(points, intrinsic):
   # Assume 3D points instead of homogeneous
   points_projected = np.matmul(intrinsic, points)
@@ -106,7 +114,7 @@ def compute_essential(cam_pose_target, points_motion):
   rotation = np.matmul(cam_rot_inv, mot_rot)
   essential = np.matmul(translation_cross, rotation)
 
-  return essential
+  return essential, translation
 
 def main():
   cam_pose_origin = np.identity(4)
@@ -115,12 +123,17 @@ def main():
   cam_mot_rpy_mean = np.array([30., 45., 60.]) # in degree 
   cam_mot_xyz_range = np.array([2., 2., 2.]) # in meter
   cam_mot_rpy_range = np.array([5., 5., 5.]) # in degree 
+  # cam_mot_xyz_mean = np.array([11., 0., 0.]) # in meter
+  # cam_mot_rpy_mean = np.array([0., 0., 0.]) # in degree 
+  # cam_mot_xyz_range = np.array([0., 0., 0.]) # in meter
+  # cam_mot_rpy_range = np.array([0., 0., 0.]) # in degree 
   cam_pose_target = gen_random_pose(cam_mot_xyz_mean, cam_mot_rpy_mean, cam_mot_xyz_range, cam_mot_rpy_range)
 
   print("Camera pose origin:")
   print(cam_pose_origin)
   print("Camera pose target:")
   print(cam_pose_target)
+  print()
 
   # # check target pose rotation
   # rot = cam_pose_target[0:3, 0:3]
@@ -132,15 +145,15 @@ def main():
 
   number_points = 8
   points_origin_centre = np.array([5., 6., 7.])
-  points_origin_range = np.array([1., 1., 1.])
+  points_origin_range = np.array([3., 3., 3.])
   points_origin = gen_random_points(number_points, points_origin_centre, points_origin_range)
 
   points_mot_xyz_mean = np.array([10., 11., 12.]) # in meter
   points_mot_rpy_mean = np.array([30., 45., 60.]) # in degree 
   points_mot_xyz_range = np.array([2., 2., 2.]) # in meter
   points_mot_rpy_range = np.array([5., 5., 5.]) # in degree 
-  # points_motion = gen_random_pose(cam_mot_xyz_mean, cam_mot_rpy_mean, cam_mot_xyz_range, cam_mot_rpy_range)
-  points_motion = np.identity(4)
+  points_motion = gen_random_pose(cam_mot_xyz_mean, cam_mot_rpy_mean, cam_mot_xyz_range, cam_mot_rpy_range)
+  # points_motion = np.identity(4)
 
   points_target = transform_points(points_origin, points_motion)
 
@@ -150,10 +163,10 @@ def main():
   print(points_target)
 
   intrinsic = np.identity(3);
-  # intrinsic[0, 0] = 320
-  # intrinsic[1, 1] = 320
-  # intrinsic[0, 2] = 320
-  # intrinsic[1, 2] = 240
+  intrinsic[0, 0] = 320
+  intrinsic[1, 1] = 320
+  intrinsic[0, 2] = 320
+  intrinsic[1, 2] = 240
 
   points_origin_local = transform_points(points_origin, np.linalg.inv(cam_pose_origin))
   points_target_local = transform_points(points_target, np.linalg.inv(cam_pose_target))
@@ -163,6 +176,11 @@ def main():
   print("Points target local:")
   print(points_target_local)
 
+  # points_origin_cv = point_frame_robotics_to_cv(points_origin_local)
+  # points_target_cv = point_frame_robotics_to_cv(points_target_local)
+
+  # depth_origin, points_origin_image = project_points(points_origin_cv, intrinsic)
+  # depth_target, points_target_image = project_points(points_target_cv, intrinsic)
   depth_origin, points_origin_image = project_points(points_origin_local, intrinsic)
   depth_target, points_target_image = project_points(points_target_local, intrinsic)
 
@@ -170,11 +188,13 @@ def main():
   print(points_origin_image)
   print("Points target image:")
   print(points_target_image)
+  print()
 
-  essential, mask = cv2.findEssentialMat(points_origin_image.T, points_target_image.T)
+  essential, mask = cv2.findEssentialMat(points_origin_image.T, points_target_image.T, intrinsic)
 
   print("Essential matrix:")
   print(essential)
+  print()
 
   R1, R2, t = cv2.decomposeEssentialMat(essential)
 
@@ -184,16 +204,29 @@ def main():
   print(R2)
   print("Translation:")
   print(t)
+  print()
 
-  test1 = np.matmul(np.linalg.inv(cam_pose_target[0:3, 0:3]), points_motion[0:3, 0:3])
+  test_rotation = np.linalg.inv(cam_pose_target[0:3, 0:3]) @ ( points_motion[0:3, 0:3] @ cam_pose_origin[0:3, 0:3])
+  # test_rotation = np.matmul(np.linalg.inv(cam_pose_target[0:3, 0:3]), np.matmul(points_motion[0:3, 0:3], cam_pose_origin[0:3, 0:3]))
 
   print("Test expected rotational component:")
-  print(test1)
+  print(test_rotation)
 
-  test2 = compute_essential(cam_pose_target, points_motion)
+  test_essential, test_translation = compute_essential(cam_pose_target, points_motion)
 
+  print("Test expected translation component:")
+  print(test_translation)
   print("Test expected essential matrix")
-  print(test2)
+  print(test_essential)
+
+  R1_test, R2_test, t_test = cv2.decomposeEssentialMat(test_essential)
+
+  print("Rotation 1 test:")
+  print(R1_test)
+  print("Rotation 2 test:")
+  print(R2_test)
+  print("Translation test:")
+  print(t_test)
 
 if __name__ == "__main__":
   main()
