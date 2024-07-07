f80efe7fbe76534920d159faf48725125f1544e6
Jesse Morris <jessmmorris7@gmail.com>
2024-07-08 07:39:29 +1000

    changed for omd reconstrcution

---
+++ dynosam_utils/src/run_eval.py
@@ -0,0 +1,30 @@
+from eval_launch import run
+
+def run_sequnce(path, name, data_loader_num):
+    parsed_args = {
+        "dataset_path": path,
+        "output_path": "/root/results/DynoSAM/",
+        "name": name,
+        "run_pipeline": True,
+        "run_analysis": True,
+        "launch_file": "dyno_sam_launch.py"
+    }
+
+    additional_args = [
+        "--backend_updater_enum=0",
+        f"--data_provider_type={data_loader_num}",
+        "--use_backend=1",
+        "--save_frontend_json=false",
+        f"--constant_object_motion_translation_sigma=0.3",
+        f"--constant_object_motion_rotation_sigma=0.1",
+        f"--ending_frame=500"
+    ]
+
+    run(parsed_args, additional_args)
+
+if __name__ == '__main__':
+    run_sequnce(
+        "/root/data/omm/swinging_4_unconstrained",
+        "swinging_4_unconstrained",
+        3
+    )
